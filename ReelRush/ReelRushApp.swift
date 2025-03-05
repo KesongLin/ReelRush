a8819de834147ead4c8cf4742c331840d64477bb
Kesong Lin <kvl5742@psu.edu>
2025-03-05 00:15:32 -0500

    Initial Commit

---
+++ ReelRush/ReelRushApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReelRushApp.swift
+//  ReelRush
+//
+//  Created by Kesong Lin on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ReelRushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReelRushTests/ReelRushTests.swift
@@ -0,0 +1,16 @@
+//
+//  ReelRushTests.swift
+//  ReelRushTests
+//
+//  Created by Kesong Lin on 3/5/25.
+//
+
+import Testing
+
+struct ReelRushTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
